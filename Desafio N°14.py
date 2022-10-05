5e95cce8a55e433e4049481d721afd84d8eda578
EduRePiN <96556245+EduRePiN@users.noreply.github.com>
2022-10-05 18:32:13 -0300

    Desafío 14

---
+++ Desafio N°14.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  3 19:18:03 2022
+
+@author: Eduardo
+"""
+
+#Desafío 14:
+#Realizar un programa que cargue 5 personas con sus datos, nombre, apellido y edad.
+#Se debe almacenar en un diccionario y luego escribir un archivo separado por comas con los datos de cada registro del diccionario.
+
+persona = {
+    "nombre" : [],
+    "apellido" : [],
+    "edad" : [],
+    }
+
+for i in range(5):
+    nombre = input("Ingrese el nombre: ")
+    persona["nombre"].append(nombre)
+    apellido = input("Ingrese el apellido: ")
+    persona["apellido"].append(apellido)
+    edad = input("Ingrese la edad: ")
+    persona["edad"].append(edad)
+
+
+contenido = open("D:\CILSA\Python\Desafios_Python_CILSA\personas.txt", "w")
+
+for i in range(5):
+    contenido.write(persona["nombre"] [i] + " , ")
+    contenido.write(persona["apellido"] [i] + " , ")
+    contenido.write(persona["edad"] [i] + "\n")
+
+contenido.close()
